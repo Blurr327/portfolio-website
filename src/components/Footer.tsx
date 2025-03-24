eecc00a0ca8553862e7ec087c09089cdb578ae49
Mohamed BEN EL MOSTAPHA <benelmed36@gmail.com>
2025-03-24 15:14:27 +0100

    sig.png

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,13 @@ import Socials from "./Socials";
 
 const Footer = ({ socials }: { socials: SocialsData }) => {
   return (
-    <footer className="px-10 border-t grow-0 border-t-zinc-500 py-10 flex md:flex-row items-center justify-between">
-      <Socials socials={socials} size={32} className="flex flex-wrap gap-4" />
-      <p className="text-sm text-zinc-500 mt-4 md:mt-0">
+    <footer className="px-10 border-t grow-0 border-t-zinc-500 py-10 flex-row items-center justify-between">
+      <Socials
+        socials={socials}
+        size={32}
+        className="flex flex-wrap justify-center gap-4 my-2"
+      />
+      <p className="text-sm text-zinc-500 text-center mt-4 md:mt-0">
         &copy; {new Date().getFullYear()} BEN EL MOSTAPHA Mohamed. All rights
         reserved.
       </p>
