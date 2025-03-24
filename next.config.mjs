c240b1ab45129b3cfd71eb70394ab1b4ac696ae3
Mohamed BEN EL MOSTAPHA <benelmed36@gmail.com>
2025-03-24 14:14:42 +0100

    fix: update nextjs config for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   trailingSlash: true,
+  basePath: isProd ? "/portfolio-website" : "",
+  assetPrefix: isProd ? "/portfolio-website/" : "",
 
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
