742d5d5d937b38240cbb12cce280e02c03d49d69
Mohamed BEN EL MOSTAPHA <benelmed36@gmail.com>
2025-03-24 14:53:23 +0100

    fix: remove previous next js config

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,6 @@ const nextConfig = {
   output: "export",
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   trailingSlash: true,
-  basePath: isProd ? "/portfolio-website" : "",
-  assetPrefix: isProd ? "/portfolio-website/" : "",
 
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
